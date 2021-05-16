2d5e42b786e1b9f20e0108c085f23f6ecfed4893
Annie <annielin28815@gmail.com>
2021-05-16 19:40:24 +0800

    modify the style of memo app

--- practice-memo-app/script.js
+++ practice-memo-app/script.js
@@ -8,10 +8,14 @@ const list = document.getElementById('list')
 
 const listContent = []
 
+// 把功能包成一個class，讓新增和刪除共用，再render出去。（把很多的功能包裝起來時可以用） 
+
 // class RenderFeature{
+// 功能一
 //   append() {
 
 //   }
+//  功能二
 //   render() {
 //     // 渲染頁面的list
 //     let htmlStr = ''
@@ -32,12 +36,15 @@ const listContent = []
 // }
 
 
-// function 
+// 把功能包成一個function，讓新增和刪除共用，再render出去。（把渲染的邏輯包裝起來，節省程式碼） 
 function render() {
   // 渲染頁面的list
   let htmlStr = ''
 
+  // 透過迴圈的方式，把陣列裡的東西逐一遍歷出來。
+  // 所以原本空的陣列，會因為丟進去的東西有幾個，就跑幾次。
   listContent.forEach(function (item) {
+    // 反引號稱做模版語法，並可透過$號放入變數
     htmlStr = htmlStr + `
     <div class="item">
       <div>
@@ -49,6 +56,7 @@ function render() {
     `
   })
 
+  // 會取代原本HTML的內容
   list.innerHTML = htmlStr
 }
 
@@ -56,6 +64,8 @@ function render() {
 
 addedBtn.addEventListener('click', function () {
 
+  // push ->  是從後面推東西進去（越新越後面）
+  // unshift -> 是從前面推東西進去（越新越前面）
   listContent.unshift({
     title: title.value,
     content: content.value,
@@ -68,6 +78,9 @@ addedBtn.addEventListener('click', function () {
 })
 
 deletedBtn.addEventListener('click', function () {
+
+  // pop ->  是從後面推東西進去（越新越後面）
+  // shift -> 是從前面推東西進去（越新越前面）
   listContent.shift()
 
   render()
